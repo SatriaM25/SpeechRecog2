35e92552cff62402bebcbfe1ac6b3aa806fa61dc
SatriaM25 <satriamuhammad11111@gmail.com>
2023-04-10 22:32:45 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
